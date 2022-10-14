2ba9e35dc49a7b0faba2e8b6e12491449aadcbce
GulSam00 <gulsamcono@gmail.com>
2022-10-14 14:47:57 +0900

    Fix : REACT_APP 추가

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
   function getHello() {
     const greet = document.getElementById("greet");
-    fetch(process.env.LOCAL_API + "/api/hello")
+    fetch(process.env.REACT_APP_LOCAL_API + "/api/hello")
       .then((response) => response.json())
       .then((data) => (greet.innerHTML = JSON.stringify(data)));
   }
