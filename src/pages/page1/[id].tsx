c942fa94e92cf1bd3c5e5d5d79ec8a484d6c0402
GulSam00 <gulsamcono@gmail.com>
2023-04-02 11:00:47 +0900

    Faet

---
+++ src/pages/page1/[id].tsx
@@ -0,0 +1,8 @@
+export default function page1() {
+  return (
+    <div>
+      page1
+      <div>1번 페이지 내부</div>
+    </div>
+  );
+}
---
+++ src/pages/page1/index.tsx
@@ -0,0 +1,8 @@
+export default function page1() {
+  return (
+    <div>
+      page1
+      <div>1번 페이지 메인</div>
+    </div>
+  );
+}
