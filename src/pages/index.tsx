a93e395d824cd223bafd7fad05a2edd0804b4736
GulSam00 <gulsamcono@gmail.com>
2023-04-09 00:54:40 +0900

    2주차

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ db }: any) {
   return (
     <div>
       test
@@ -21,6 +21,25 @@ export default function Home() {
           <Link href="/getServerSideProps">getServerSideProps</Link>
         </h1>
       </div>
+      <li>db: {db.host}</li>
+      <li>username: {db.username}</li>
+      <li>password: {db.password}</li>
+      <li>password: {db.password}</li>
+      <li> {db.testInclude}</li>
+      <li> {db.testExpect}</li>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const db = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    test: process.env.TEST,
+    testInclude: process.env.TEST_INCLUDE,
+    testExpect: process.env.TEST_INCLUDE_EXCEPT,
+  };
+
+  return { props: { db } };
 }
